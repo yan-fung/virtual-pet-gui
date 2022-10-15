50804c83d90c6dafbeaa5c30ea9292a38dbac153
Yan Fung <fungmingyan@hotmail.com>
2022-10-15 16:17:36 +0100

    added growUp as a method on Pet's prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,11 +7,25 @@ describe('constructor', () => {
 });
 
 describe('constructor', () => {
-    
-  
     it('sets the name property', () => {
       const pet = new Pet('Fido');
-  
       expect(pet.name).toEqual('Fido');
+    });
+  });
+
+  describe('constructor', () => {
+    it('has a inital age of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
     });
   });
